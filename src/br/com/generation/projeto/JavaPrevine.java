a47c8654c1427bfb26e5e45df949c05e818b5fbb
igoryouiti <igoryouiti@hotmail.com>
2022-07-13 21:51:03 -0300

    inserindo classes no main e executando seus metodos dentro do menu

--- src/br/com/generation/projeto/JavaPrevine.java
+++ src/br/com/generation/projeto/JavaPrevine.java
@@ -10,32 +10,64 @@ public class JavaPrevine {
 		int opcao;
 		
 		Scanner entrada = new Scanner(System.in);
+		MenuPrincipal menu = new MenuPrincipal(entrada);
+		CalculadoraCovid calculadora = new CalculadoraCovid(entrada);
+		Diagnostico diagnostico = new Diagnostico(entrada);
 		
 		
 		// looping do programa
 		do {
 			
-			System.out.println("MENU - SISTEMA");
-
-			System.out.println("1 - Calculadora Covid");
-			System.out.println("2 - Diagnóstico");
-			System.out.println("0 - Para sair");
-			System.out.print("Digite uma opção válida: ");
-			opcao = entrada.nextInt();
-
-			
+			opcao = menu.menuPrincipal();
 			
 			switch(opcao) {
 			// Calculadora
 			case 1:
-				System.out.println("Bem vindo a Calculadora");
+				
+				// loop menu calculadora
+				do {
+					
+					opcao = calculadora.menuCovid();					
+					
+					switch(opcao) {
+					
+					case 1:
+						System.out.println("Quantas pessoas posso contaminar");
+					
+						break;
+					
+					
+					case 2:
+						System.out.println("N pessoas contaminam quantas pessoas em certo periodo?");
+						
+						break;
+						
+					case 3:
+						System.out.println("Faca seu próprio calculo");
+						
+						break;
+						
+					// Sair
+					case 0:
+						
+						System.out.println("AUA FUI!");
+						
+						break;
+					
+					default:
+						System.out.println("Opcao inválida!");
+						break;
+					}
+					
+
+				}while(opcao != 0);	
 			
 				break;
 			
 			// Diagnostico
 			case 2:
 				System.out.println("Bem vindo ao Diagnóstico");
-				
+					System.out.println(diagnostico.resultadoFinal()); 
 				break;
 				
 			// Sair
